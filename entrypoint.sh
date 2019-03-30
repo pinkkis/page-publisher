9acb52427cc67ff62bf822452ca74d7cee6ceabb
Kristian Koivisto-Kokko <pinkkis@gmail.com>
2019-03-30 14:04:36 +0000

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+REF=$1
+if [ -z $1 ]; then
+    REF='master'
+fi
+
+if [ -z "${TOKEN}" ]; then
+  TOKEN=${GITHUB_TOKEN}
+fi
+
+if [ -z "${TARGET_REPO}" ]; then
+  TARGET_REPO=${GITHUB_REPOSITORY}
+fi
+
+REMOTE_REPO="https://${TOKEN}@github.com/${TARGET_REPO}.git"
+
+cd "${GITHUB_WORKSPACE}" || exit 1
+git submodule update --init --recursive
+
+rm -rf .git
+cd public
+
+if [ -z "${CNAME}" ]; then
+    echo "${GITHUB_ACTOR}.github.io" > CNAME
+else
+    echo ${CNAME} > CNAME
+fi
+
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add .
+git commit -m 'Auto Published From Action'
+git push --force $REMOTE_REPO master:$REF
